cb5a9832dbec349d7b8c6c7d149dafee6e09b668
vhqr0 <zq_cmd@163.com>
2023-02-14 09:37:18 +0800

    nmap: update arg parser

--- nmap.py
+++ nmap.py
@@ -7,17 +7,21 @@ from pynmap6.port_scan import PortScanner
 from pynmap6.target_generate import TargetGenerator
 
 parser = argparse.ArgumentParser()
-parser.add_argument('-i', '--iface', default=sp.conf.iface)
+parser.add_argument('-i', '--iface', default=str(sp.conf.iface))
 parser.add_argument('-p', '--ports', default='22,80,443')
+parser.add_argument('-I', '--interval', type=float, default=1.0)
 parser.add_argument('addrs', nargs=argparse.REMAINDER)
 args = parser.parse_args()
 
 iface = args.iface
-ports = args.ports.split(',')
 addrs = args.addrs
+ports = args.ports.split(',')
+interval = args.interval
+
+sp.conf.iface = iface
 
 targets = TargetGenerator(addrs, ports).new()
-scanner = PortScanner(targets, iface=iface)
+scanner = PortScanner(targets, interval=interval)
 
 scanner.run()
 results = scanner.parse()
